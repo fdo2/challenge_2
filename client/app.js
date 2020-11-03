601cc7cf71f191deba551680d3fa79354004520d
fdo2 <fdo2@live.com>
2020-11-03 19:12:11 +0200

    Really things occured ,, happy .. Hola git add .

--- client/app.js
+++ client/app.js
@@ -1,7 +1,19 @@
 
+import $ from "jquery";
 
     function reset() {
       document.getElementById('form').reset();
     }
 
-    
+
+    function downlaod() {
+      myJson = {
+    Fullname: $("#form").find("#name").val(),
+    Country: $("#form").find("#country").val(),
+    City: $("#form").find("#city").val(),
+    Role: $("#form").find("#role").val(),
+    Sales: $("#form").find("#sales").val(),
+  };
+
+  console.log("myJson", myJson);
+  }
--- server.js
+++ server.js
@@ -1,2 +1,17 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+const port = process.env.PORT || 1990;
+var favicon = require('serve-favicon');
 
+app.listen(port, () => {
+  console.log(`Server is listening to http://localhost:${port}`)
+})
 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/client/index.html');
+});
+
+app.use(express.static(path.join(__dirname, 'client')));
+app.use(favicon(__dirname + '/client/favicon.ico'));
